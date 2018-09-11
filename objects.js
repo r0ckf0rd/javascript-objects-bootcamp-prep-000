6a7c55c55cc3ec3d5e2da7cea82c10e7f09c7bc1
Nicholas Werner <micropacman@gmail.com>
2018-09-11 20:22:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {Thrice: `Beggars`}}
+var playlist = {Thrice: `Beggars`}
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName]: songTitle})
